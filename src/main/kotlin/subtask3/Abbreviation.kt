fb52ab9e23fb6d074c42ff17e88d6d0bee9c6bf9
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-04-14 20:19:05 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,13 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val a1 = a.toUpperCase().toMutableList()
+        val b1 = b.toMutableList()
+        val newA1 = a1.intersect(b1).toMutableList()
+            return when {
+                (newA1 == b1) -> "YES"
+                else -> "NO"
+            }
     }
 }
