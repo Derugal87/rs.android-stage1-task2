dfc6d8f2eb8b82d85c3a077200ae359cda15d354
Derugal87 <60985658+Derugal87@users.noreply.github.com>
2020-04-14 20:18:14 +0300

    Update TimeConverter.kt

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,54 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val timeArray = arrayOf(
+            "",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "quarter",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine",
+            "half"
+        )
+
+        var h = hour.toInt()
+        var m = minute.toInt()
+
+        return when(m) {
+            0 -> timeArray[h] + " o' clock"
+            1 -> timeArray[0] + " minute to " + timeArray[h]
+            15 -> timeArray[m] + " past " + timeArray[h]
+            30 -> timeArray[m] + " past " + timeArray[h]
+            in 2..29 -> timeArray[m] + " minutes past " + timeArray[h]
+            59 -> timeArray[0] + " minute to " + timeArray[h + 1]
+            45 -> timeArray[60 - m] + " to " + timeArray[h + 1]
+            in 31..58 -> timeArray[60 - m] + " minutes to " + timeArray[h + 1]
+            else -> ""
+        }
     }
 }
